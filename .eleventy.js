61ce94fb2ed90e41749b229b3b1f634c839f00ff
Carl Evans <carl.evans.seo@gmail.com>
2020-05-11 01:03:41 +0100

    minified js and added html minifier

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,22 @@
 const CleanCSS = require("clean-css");
+const htmlmin = require("html-minifier");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("fonts");
   eleventyConfig.addFilter("cssmin", function(code) {
     return new CleanCSS({}).minify(code).styles;
+  });
+  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
+    if( outputPath.endsWith(".html") ) {
+      let minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+
+      return minified;
+    }
+
+    return content;
   });
 };
